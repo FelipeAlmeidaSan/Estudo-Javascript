bc4ee389961d4fd97f7d990392aa86744e4c3370
Felipe Almeida <felipealmeidaux@gmail.com>
2025-06-27 19:22:05 -0300

    Fundamentos javascript

---
+++ javascript/dayOne/dayone.js
@@ -0,0 +1,60 @@
+// Variavéis
+
+let name = "João";     // Pode ser alterado depois
+const age = 25;        // Valor fixo, nõa pode mudar
+var city = "Pelotas"   // Antiga forma de declarar variáveis
+
+
+// Tipos de Dados
+
+String    // "texto"
+Number    //  10 , 3,534
+Boolean   //  true ou false
+Array     //  [1,3,5,7]
+Object    //  {nome: Felipe}
+null      //  Ausência de valor
+undefined //  Valor nõa definido
+
+// Exemplo de tipos de dados
+
+let nome = "Felipe";
+let idade = 30;
+let ativo = true;
+let lista = [1, 5, 9];
+let usuário = { nome: "Ana", idade: 20 };
+
+// Operadores
+
+// --- Aritméticos
+
+// +  Soma
+// -  Subtração
+// *  Multiplicação
+// /  Divisão
+// %  Resto da divisão
+
+// --- De comparação
+
+// ==     Igual (valor)
+// ===    Estritamente igual (valor e tipo)
+// !=     Diferente
+// !==    Estritamente diferente
+// >      Maior que
+// <      Menor que
+// >=     Maior ou igual
+// <=     Menor ou igual
+
+
+// Lógicos
+
+// &&   E
+// ||   Ou 
+// !    Não
+
+// Exemplo:
+
+let a = 10;
+let b = 30;
+
+console.log(a < b);
+console.log(a < b && b === 30); 
