88697f98be555720df95d80c0847ad35fb92bd07
Felipe Almeida <felipealmeidaux@gmail.com>
2025-06-28 07:24:07 -0300

    Exercício 1 Variáveis

--- javascript/dayOne/exercice.js
+++ javascript/dayOne/exercice.js
@@ -1,2 +1,15 @@
-console.log("hello world")
-document.getElementById("parágrafo").innerHTML;
+let nome = "joão Felipe"
+let anoNascimento = 2002
+let idade
+let cidade = "Aracaju"
+
+console.log(2025 - anoNascimento);
+
+
+/* Crie uma variável chamada nome e atribua o seu nome a ela.
+
+Crie uma variável anoNascimento e atribua o ano em que você nasceu.
+
+Crie uma variável idade e calcule a idade com base no ano atual (ex: 2025 - anoNascimento).
+
+Crie uma variável cidade e altere seu valor depois.  */
