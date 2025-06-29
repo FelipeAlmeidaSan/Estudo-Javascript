b4f00eb948a4099a5f0ef13e9c78df5ab4369c8b
Felipe Almeida <felipealmeidaux@gmail.com>
2025-06-29 10:06:31 -0300

    Uso de template string, um recurso que deixa o código moderno e elegante.

--- javascript/dayOne/exercice.js
+++ javascript/dayOne/exercice.js
@@ -13,15 +13,21 @@ console.log("Cidade:", cidade);
 cidade = "São Paulo"
 console.log("cidade atualizada:", cidade);
 
-console.log("Olá, meu nome é", nome, ",tenho", idade, "Nasci em", anoNascimento, "na cidade de aracaju e hoje moro em", cidade);
+// Tamplate string
 
+console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos.`);
+console.log(`Nasci em ${diaNascimento} de ${mesNascimento} de ${anoNascimento}.`);
+console.log(`Nasci na cidade de Aracaju e atualmente moro em ${cidade}.`);
 
+/*
 
-
-/* Crie uma variável chamada nome e atribua o seu nome a ela.
+Crie uma variável chamada nome e atribua o seu nome a ela.
 
 Crie uma variável anoNascimento e atribua o ano em que você nasceu.
 
 Crie uma variável idade e calcule a idade com base no ano atual (ex: 2025 - anoNascimento).
 
-Crie uma variável cidade e altere seu valor depois.  */
+Crie uma variável cidade e altere seu valor depois. 
+
+
+*/
