b2bcb087d6fea453af40d7a5a49f50d3c6161f8e
Felipe Almeida <felipealmeidaux@gmail.com>
2025-07-03 10:03:02 -0300

    Correção segundo exercício

--- src/javascript/ExercíciosDayOne.js
+++ src/javascript/ExercíciosDayOne.js
@@ -3,11 +3,7 @@
 let nome = "Edilson"
 let idade = 53
 let cidade = "Itabaiana"
-let profissão = "açogueiro"
-
-let texto = `Olá, meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissão}`;
-
-document.getElementById("resultado").innerHTML = "texto"
+let profissão = "açougueiro"
 
 console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissão}`);
 
@@ -15,3 +11,26 @@ console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade} e
 // nome, idade, cidade, profissão
 // Depois use um console.log para escrever:
 // "Olá, meu nome é João, tenho 25 anos, moro em São Paulo e sou designer."
+
+
+
+// Segundo Exercício
+
+let produto = {
+    nome: "smartphone",
+    preço: 499,
+    disponibilidade: true,
+}
+
+if (produto.disponibilidade = true) {
+    console.log(`O produto ${produto.nome} está disponível por ${produto.preço} reais.`);
+} else {
+    console.log("Produto indisponível no momento.");
+}
+
+
+// Crie um objeto 'produto' com nome, preço e disponibilidade (boolean).
+// Crie uma condicional que verifica se o produto está disponível
+// Se sim, exiba: "O produto NOME está disponível por PREÇO reais."
+// Se não, exiba: "Produto indisponível no momento."
+
