0621fd23813723ee76e5bde5e1985cbe8aefc843
Felipe Almeida <felipealmeidaux@gmail.com>
2025-07-03 09:07:02 -0300

    Adição de novas pastas

---
+++ src/javascript/ExercíciosDayOne.js
@@ -0,0 +1,13 @@
+// Primeiro Exercício
+
+let nome = "Edilson"
+let idade = 53
+let cidade = "Itabaiana"
+let profissão = "açogueiro"
+
+console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissão}`)
+
+// Crie variáveis para armazenar:
+// nome, idade, cidade, profissão
+// Depois use um console.log para escrever:
+// "Olá, meu nome é João, tenho 25 anos, moro em São Paulo e sou designer."
