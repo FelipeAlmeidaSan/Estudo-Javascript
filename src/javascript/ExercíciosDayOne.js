333cd598cdc56d964870286b499f08d6f0708705
Felipe Almeida <felipealmeidaux@gmail.com>
2025-07-03 09:23:21 -0300

    Primeiro exercício, declaração de variáveis

--- src/javascript/ExercíciosDayOne.js
+++ src/javascript/ExercíciosDayOne.js
@@ -5,7 +5,11 @@ let idade = 53
 let cidade = "Itabaiana"
 let profissão = "açogueiro"
 
-console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissão}`)
+let texto = `Olá, meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissão}`;
+
+document.getElementById("resultado").innerHTML = "texto"
+
+console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissão}`);
 
 // Crie variáveis para armazenar:
 // nome, idade, cidade, profissão
