4cfb766bc09d23c53b93a3d34add0a1655aed6a0
Felipe Almeida <felipealmeidaux@gmail.com>
2025-07-05 06:56:15 -0300

    Segundo exercício, função com parâmetro

--- src/javascript/ExerciceDayTwo.js
+++ src/javascript/ExerciceDayTwo.js
@@ -2,3 +2,16 @@
 function mostrarMensagem() {
     console.log("Bem-vindo ao sistema, Felipe!")
 }
+
+mostrarMensagem();
+
+// Crie uma função chamada boasVindas que recebe um nome como parâmetro e exibe:
+function boasVindas(nome) {
+    console.log(`Olá, ${nome}, seja bem-vindo!`)
+}
+
+boasVindas("Maria")
+boasVindas("João")
+boasVindas("Marcos")
+
+
