7b70dc56efe1ed3bfa242a993ce592c51dcca9ea
Felipe Almeida <felipealmeidaux@gmail.com>
2025-07-05 07:00:23 -0300

    Parâmetros Rest

--- src/javascript/ExerciceDayTwo.js
+++ src/javascript/ExerciceDayTwo.js
@@ -15,3 +15,14 @@ boasVindas("João")
 boasVindas("Marcos")
 
 
+// Parâmetros Rest:
+function somar(...numeros) {
+    let soma = 0;
+    for (let numero of numeros) {
+        soma += numero;
+    }
+    return soma;
+}
+
+console.log(somar(1, 2, 3)); // Saída: 6
+console.log(somar(10, 20, 30, 40, 50)); // Saída: 150
