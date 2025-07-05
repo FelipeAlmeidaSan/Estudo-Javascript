540c5115be9b6cc1b13bdcd37d47401261e377b6
Felipe Almeida <felipealmeidaux@gmail.com>
2025-07-04 22:21:00 -0300

    Estudo de paâmetros e retorno

--- src/javascript/DayTwo.js
+++ src/javascript/DayTwo.js
@@ -7,9 +7,15 @@ function dizerOla() {
 dizerOla(); // Chamada da função
 
 
+// Parâmetros 
+/* Parâmetros são valores que você passa para a função usar internamente. */
+
 function saudar(nome) {
   console.log(`Olá, ${nome}!`);
 }
 
 saudar("João"); // Saída: Olá, João!
 saudar("Ana");  // Saída: Olá, Ana!
+
+// Retorno (return)
+
